7e32c789944bda799846531dce696295bc99ed4b
Yotzapon <yotsapon@haii.or.th>
2015-10-22 15:46:31 +0700

    Init Proj

---
+++ assets/js/lib/nhc_map.js
@@ -0,0 +1,50 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: yotzapon
+ * Date: 10/22/15
+ * Time: 11:35 AM
+ */
+
+function initmap() {
+
+    //L.mapbox.accessToken = '<your access token here>';
+
+    // set up the map
+    //map = new L.Map('map');
+    var map = L.map('map').setView([13.4925449478, 100.329052521], 6);
+
+    //BBox Bangkok
+    //x_min 	    x_max 	        y_min 	        y_max
+    //100.329052521   100.938572013   13.4925449478   13.9552690081
+
+    var southWest = L.latLng(13.4925449478, 100.329052521),
+        northEast = L.latLng(13.9552690081, 100.938572013),
+        bounds = L.latLngBounds(southWest, northEast);
+
+    var osm = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieW90emEiLCJhIjoiY2lnMXNpYmZuMTJwYXVybTNvbjJxbjNvciJ9.PVXO9b4mzrd87l_yIUk3nw', {
+        maxZoom: 18,
+        minZoom: 10,
+        maxBounds: bounds,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
+    });
+
+
+
+    map.fitBounds(bounds);
+
+    map.addLayer(osm);
+
+
+
+    // create the tile layer with correct attribution
+    //var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    //var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    //var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+    //
+    //// start the map in South-East England
+    //map.setView(new L.LatLng(51.3, 0.7),9);
+    //map.addLayer(osm);
+}
---
+++ src/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Mapbox Styles -->
+    <link rel="stylesheet" type="text/css" href="assets/js/lib/mapbox.js/mapbox.css" />
+
+    <!-- Leaflet Styles -->
+    <link rel="stylesheet" type="text/css" href="assets/js/lib/leaflet/dist/leaflet.css" />
+
+    <!-- Site Styles -->
+    <style>
+        html, body, #map {
+            height:100%;
+            width:100%;
+            padding:0px;
+            margin:0px;
+        }
+    </style>
+
+    <title>NHC-IGIS</title>
+</head>
+<body>
+
+    <div id="map"></div>
+
+    <!-- jQuery JS -->
+    <script type="text/javascript" src="assets/js/lib/jquery/dist/jquery.min.js"></script>
+
+    <!-- Mapbox JS -->
+    <script type="text/javascript" src="assets/js/lib/mapbox.js/mapbox.js"></script>
+
+    <!-- Leaflet JS -->
+    <script type="text/javascript" src="assets/js/lib/leaflet/dist/leaflet.js"></script>
+
+    <!-- Site JS -->
+    <script src="assets/js/lib/nhc_map.js"></script>
+
+    <script type="text/javascript">
+
+        $(document).ready(function() {
+            initmap();
+        });
+
+    </script>
+</body>
+</html>
